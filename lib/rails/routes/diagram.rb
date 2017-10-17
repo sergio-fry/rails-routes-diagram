449cc8a2f4b9938a538c81082dd7216502d8b8c0
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-10-17 16:02:55 +0300

    init

---
+++ lib/rails/routes/diagram.rb
@@ -0,0 +1,9 @@
+require "rails/routes/diagram/version"
+
+module Rails
+  module Routes
+    module Diagram
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/routes/diagram/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Routes
+    module Diagram
+      VERSION = "0.1.0"
+    end
+  end
+end
